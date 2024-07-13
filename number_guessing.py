e552d5f9a6d3441f377b274ab5000154b609d277
Vani A S <vaniagasanakatti01@gmail.com>
2024-07-13 18:53:20 +0530

    Add files via upload

---
+++ number_guessing.py
@@ -0,0 +1,34 @@
+import random
+
+top_of_range=input("type the number:")
+
+if top_of_range.isdigit():
+    top_of_range=int(top_of_range)
+    
+    if top_of_range<=0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("please type a number next time")
+    quit()
+    
+random_number=random.randint(0,top_of_range)
+
+
+while True:
+    user_guess=input("make a guess:")
+    if user_guess.isdigit():
+        user_guess=int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+    
+    if user_guess==random_number:
+        print("You got it!")
+        break
+    elif user_guess> random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+print("You got it in",str(user_guess),"guesses")
+print(random_number)
